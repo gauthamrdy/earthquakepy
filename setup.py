6eecd79dfeb1ac53c679c4de3dd9627a305268bf
Digvijay Patankar <dbpatankar@gmail.com>
2022-04-02 00:51:22 +0530

    version 0.2.2
    Added pypi package file and setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ from setuptools import setup
 
 setup(
     name="earthquakepy",
-    version="0.2.1",
+    version="0.2.2",
     description="python library for earthquake engineers.",
     url="https://github.com/gauthamrdy/earthquakepy",
     author="Gautham Reddy, Digvijay Patankar",
     author_email="pgrddy@gmail.com, dbpatankar@gmail.com",
     license="GNU GPLv3",
     packages=["earthquakepy"],
-    install_requires=["numpy",
-                      "scipy",
-                      "matplotlib",
-                      ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+    ],
     python_requires=">=3.6",
-
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
